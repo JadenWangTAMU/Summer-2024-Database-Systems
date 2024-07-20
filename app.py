831fe18840fd08595d05d3422efd70d8bbe114da
JadenWangTAMU <142842705+JadenWangTAMU@users.noreply.github.com>
2024-07-20 15:57:31 -0500

    Update app.py
    Adding additional default stuff

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from multiprocessing import synchronize
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select
+from sqlalchemy import exc
+import psycopg2
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:gekkouga658Postgres@localhost/artfolio_db' #replace with your own file
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'secret string'
+
+db = SQLAlchemy(app)
+app.app_context().push()
 
 @app.route("/")
 def hello():
