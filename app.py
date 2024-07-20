16ea89d76fcd52191b0e93da6c295bb8ac7e2716
JadenWangTAMU <142842705+JadenWangTAMU@users.noreply.github.com>
2024-07-20 16:31:59 -0500

    Update app.py
    Adding default entities and their attributes

--- app.py
+++ app.py
@@ -16,3 +16,37 @@ app.app_context().push()
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+class Art_Piece(db.Model):
+    piece_id=db.Column(db.Integer, primary_key=True)
+    creator_id = db.Column(db.Integer, db.ForeignKey('Creator.Creator_ID'))
+    user_id = db.Column(db.Integer, db.ForeignKey('User.User_ID'))
+    title=db.Column(db.String(100))
+    year_finished=db.Column(db.Integer)
+    cost=db.Column(db.Float)
+    description=db.Column(db.String(100))
+    photo_link=db.Column(db.String(200))
+    sellable=db.Column(db.Boolean)
+    viewable=db.Column(db.Boolean)
+
+class Creator(db.Model):
+    creator_id=db.Column(db.Integer, primary_key=True)
+    creator_fname=db.Column(db.String(100))
+    creator_lname=db.Column(db.String(100))
+    birth_country=db.Column(db.String(100))
+    birth_date=db.Column(db.String(100))
+    death_date=db.Column(db.String(100))
+
+class User(db.Model):
+    user_id=db.Column(db.Integer, primary_key=True)
+    user_fname=db.Column(db.String(100))
+    user_lname=db.Column(db.String(100))
+    email=db.Column(db.String(100))
+    password=db.Column(db.String(100))
+    role=db.Column(db.String(100))
+
+class Transaction(db.Model):
+    transaction_id=db.Column(db.Integer, primary_key=True)
+    buyer_id = db.Column(db.Integer, db.ForeignKey('User.User_ID'))
+    seller_id = db.Column(db.Integer, db.ForeignKey('User.User_ID'))
+    timestamp=db.Column(db.Datetime)
