ed99c423e4cfc61348cab38b0d7fcbf9d7da7c44
Unknowinglyy <unknowingly@tamu.edu>
2024-07-23 16:40:10 -0500

    adding sql script and tweaking of the python script

--- app.py
+++ app.py
@@ -3,20 +3,15 @@ from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import select
 from sqlalchemy import exc
+from datetime import datetime
 import psycopg2
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:p@ssword!@localhost/artfolio' #replace with your own password
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password!@localhost:5432/artfolio'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'secret string'
 
 db = SQLAlchemy(app)
-app.app_context().push()
-
-@app.route("/")
-def hello():
-    return "Hello, World!"
-
 
 class art_piece(db.Model):
     piece_id=db.Column(db.Integer, primary_key=True)
@@ -42,16 +37,20 @@ class user(db.Model):
     user_id=db.Column(db.Integer, primary_key=True)
     user_fname=db.Column(db.String(100))
     user_lname=db.Column(db.String(100))
-    email=db.Column(db.String(100))
+    email=db.Column(db.String(100), unique=True)
     password=db.Column(db.String(100))
-    role=db.Column(db.String(100))
+    role=db.Column(db.String(1))
 
 class transaction(db.Model):
     transaction_id=db.Column(db.Integer, primary_key=True)
     piece_id = db.Column(db.Integer, db.ForeignKey('art_piece.piece_id'))
     buyer_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
     seller_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
-    timestamp=db.Column(db.DateTime)
+    timestamp=db.Column(db.DateTime, default=datetime.utcnow)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
