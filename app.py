9da5c8c1936c665c843c3e910f62fc92b88c95f0
Unknowinglyy <unknowingly@tamu.edu>
2024-07-23 18:24:33 -0500

    might work? idk

--- app.py
+++ app.py
@@ -52,5 +52,10 @@ class transaction(db.Model):
 def hello():
     return "Hello, World!"
 
+def init_db():
+    with app.app_context():
+        db.create_all()
+
 if __name__ == '__main__':
+    init_db()
     app.run(debug = True)
