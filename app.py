d068b3ed02f421c84f1ffaf2af8393719639e012
JadenWangTAMU <142842705+JadenWangTAMU@users.noreply.github.com>
2024-07-25 20:07:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -86,6 +86,75 @@ def paintings():
     #render the paintings page with all the required info
     return render_template("paintings.html", paintings = paintings.items, creators = all_creators, pagination = paintings, query=query, sort_by = sort_by)
 
+@app.route('/transactions', methods = ['GET'])
+def transactions():
+    #used for page stuff
+    page = request.args.get('page', 1, type=int)
+    query = request.args.get('query', '', type=str)
+    sort_by = request.args.get('sort_by', 'title', type=str)
+    #can change this to whatever, decides how many transactions are shown per page
+    per_page = 5
+    
+    # does the user want to search for a transaction?
+    if query:
+        #filter based on what they searched for
+        art_piece=art_piece.query.filter(art_piece.piece_id.ilike(f'%{transaction.piece_id}%'))
+        transactions_query = transaction.query.filter(art_piece.title.ilike(f'%{query}%'))
+    else:
+        #else just get all the transactions
+        transactions_query = transaction.query
+
+    #find out how the user wants to sort the transactions and sort accordingly
+    if sort_by == 'buyer_fname':
+        buyer=get_user_fromid(transaction.buyer_id)
+        transactions_query = transactions_query.order_by(buyer.user_fname)
+    elif sort_by == 'buyer_lname':
+        buyer=get_user_fromid(transaction.buyer_id)
+        transactions_query = transactions_query.order_by(buyer.user_lname)
+    elif sort_by == 'seller_fname':
+        seller=get_user_fromid(transaction.seller_id_id)
+        transactions_query = transactions_query.order_by(seller.user_fname)
+    elif sort_by == 'seller_lname':
+        seller=get_user_fromid(transaction.seller_id_id)
+        transactions_query = transactions_query.order_by(seller.user_lname)
+    elif sort_by == 'timestamp':
+        transactions_query = transactions_query.order_by(transaction.timestamp)
+    elif sort_by == 'title':
+        art_piece=art_piece.query.filter(art_piece.piece_id.ilike(f'%{transaction.piece_id}%'))
+        transactions_query = transactions_query.order_by(art_piece.title.ilike(f'%{query}%'))
+    
+    #paginate the paintings (basically separates them into pages)
+    transactions = transactions_query.paginate(page=page, per_page=per_page)
+    #get all the creators so we can display the artist of each painting
+    all_users = user.query.all()
+    #render the paintings page with all the required info
+    return render_template("transactions.html", transactions = transactions.items, users = all_users, pagination = transactions, query=query, sort_by = sort_by)
+
+@app.route('/users', methods = ['GET'])
+def users():
+    #used for page stuff
+    page = request.args.get('page', 1, type=int)
+    sort_by = request.args.get('sort_by', 'lname', type=str)
+    #can change this to whatever, decides how many users are shown per page
+    per_page = 5
+    
+    users_query = user.query
+    #find out how the user wants to sort the users and sort accordingly
+    if sort_by == 'lname':
+        users_query = users_query.order_by(user.user_lname)
+    elif sort_by == 'fname':
+        users_query = users_query.order_by(user.user_fname)
+    elif sort_by == 'email':
+        users_query = users_query.order_by(user.email)
+    elif sort_by == 'role':
+        users_query = users_query.order_by(user.role)
+    
+    #paginate the users (basically separates them into pages)
+    users = users_query.paginate(page=page, per_page=per_page)
+    #get all the creators so we can display the artist of each user
+    #render the users page with all the required info
+    return render_template("users.html", users = users.items, pagination = users, sort_by = sort_by)
+
 #not sure if this actually initalizes the database
 def init_db():
     with app.app_context():
